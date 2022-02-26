d55746885f4b299018179a41ddcb6fd0c38f34e2
Francesco <samplasion@gmail.com>
2022-02-26 10:35:42 +0100

    Use the various platforms' appcasts

--- lib/components/Utilities/update_scope.dart
+++ lib/components/Utilities/update_scope.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:upgrader/upgrader.dart';
 
@@ -11,11 +14,24 @@ class UpdateScope extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final appcastURL = 'https://samplasion.github.io/reaxios/appcast.xml';
-    final cfg = AppcastConfiguration(
-      url: appcastURL,
-      supportedOS: ['android'],
-    );
+    if (kIsWeb) return child;
+
+    AppcastConfiguration cfg;
+    if (Platform.isAndroid) {
+      final appcastURL = 'https://samplasion.github.io/reaxios/appcast.xml';
+      cfg = AppcastConfiguration(
+        url: appcastURL,
+        supportedOS: ['android'],
+      );
+    } else if (Platform.isIOS) {
+      final appcastURL = 'https://samplasion.github.io/reaxios/appcast_mac.xml';
+      cfg = AppcastConfiguration(
+        url: appcastURL,
+        supportedOS: ['macos'],
+      );
+    } else {
+      return child;
+    }
 
     return UpgradeAlert(
       appcastConfig: cfg,
