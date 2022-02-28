717e788609d65441d2f0fb287ac966ae0b338781
Francesco <samplasion@gmail.com>
2022-02-28 19:22:05 +0100

    Add OS name to generated output

--- tools/build_macos.sh
+++ tools/build_macos.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -13,7 +13,7 @@ run="build && cleanup && copy"
 
 usage() {
     echo 
-    echo "Builds the Android APK and App Bundle"
+    echo "Builds the macOS app"
     echo "Usage: $0 [-ch]"
     echo
     echo "  -c       Only run cleanup"
@@ -35,8 +35,7 @@ done
 echo 
 echo "#############################################################"
 echo "##                                                         ##"
-echo "##                    Build Android APK                    ##"
-echo "##                 and Android App Bundle                  ##"
+echo "##                     Build macOS app                     ##"
 echo "##                                                         ##"
 echo "#############################################################"
 echo 
@@ -44,34 +43,28 @@ echo App version: $VER
 echo Build number: $NOW
 echo 
 
-_build_appbundle() {
-    echo "📦 Building the bundle..."
-    flutter build appbundle --$flavor --build-number $NOW 1> /dev/null
-}
-
-_build_apk() {
-    echo "📦 Building the APK..."
-    flutter build apk --$flavor --build-number $NOW 1> /dev/null
-}
-
 build() {
-    _build_appbundle & _build_apk
-    wait
+    echo "📦 Building the macOS app..."
+    flutter build macos --$flavor 1> /dev/null
 }
 
 cleanup() {
     echo "📁 Creating the output directory if it doesn't exist..."
-    mkdir -p $CUR/out/android
+    mkdir -p $CUR/out/macos
 
-    echo "🗑 Removing previous releases..."
-    rm -rf $CUR/out/android/*
-    rm -rf $CUR/out/android/**/*
+    echo "🗑  Removing previous releases..."
+    rm -rf $CUR/out/macos/*
+    rm -rf $CUR/out/macos/**/*
 }
 
 copy() {
     echo "📑 Copying output files from their directory to our organized directory..."
-    cp -r $CUR/build/app/outputs/flutter-apk/app-release.apk $CUR/out/android/$name-android-$VER+$NOW-$flavor.apk
-    cp -r $CUR/build/app/outputs/bundle/release/app-release.aab $CUR/out/android/$name-androidbundle-$VER+$NOW-$flavor.aab
+    Flavor=${flavor^}
+    cp -r "$CUR/build/macos/Build/Products/$Flavor/$name.app" $CUR/out/macos/$name.app
+    cd $CUR/out/macos
+    zip -9 -y -r -q $name-mac-$VER+$NOW-$flavor.zip $name.app
+    rm -rf $name.app
+    cd -
 }
 
 eval $run
