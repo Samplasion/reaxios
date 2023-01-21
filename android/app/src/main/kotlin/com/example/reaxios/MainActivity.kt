b49b5ae0092b5850d2df05d8cd66bf7241ad19fa
Samplasion <samplasion@gmail.com>
2023-01-21 18:52:27 +0100

    Change Android icons

--- android/app/src/main/kotlin/com/example/reaxios/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/reaxios/MainActivity.kt
@@ -1,6 +1,6 @@
 package org.js.samplasion.reaxios
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
