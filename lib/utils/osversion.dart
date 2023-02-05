81f6cf97019aa82d37bd74ae47972854065efb82
Francesco <samplasion@gmail.com>
2023-02-05 22:33:46 +0100

    Only load Android info on Android

--- lib/utils/osversion.dart
+++ lib/utils/osversion.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
+
 import 'package:device_info_plus/device_info_plus.dart';
 
 late AndroidDeviceInfo androidInfo;
 
 Future fetchDeviceInfo() async {
   DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-  androidInfo = await deviceInfo.androidInfo;
+  if (Platform.isAndroid) androidInfo = await deviceInfo.androidInfo;
 }
